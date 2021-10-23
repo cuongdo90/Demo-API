320c58c861b927e16f3979f72b0ed88a4a7105af
cuongdo90 <cuongcoi9001th@gmail.com>
2021-10-23 18:12:09 +0700

    fig

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
-Route::post('/products', [ProductController::class, 'store']);
+Route::post('/products/create', [ProductController::class, 'store']);
 Route::put('/products/{id}', [ProductController::class, 'update']);
 Route::delete('/products/{id}', [ProductController::class, 'destroy']);
     
